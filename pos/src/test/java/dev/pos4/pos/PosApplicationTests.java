4436edf5e20a7b49fd3e746448e14205802645e3
Titaniumfire34 <z-sap@live.com>
2023-05-01 16:52:01 -0400

    added project

---
+++ pos/src/test/java/dev/pos4/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pos4.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
