60290ef210e19b61eb8dae0966790a6f35799299
Titaniumfire34 <z-sap@live.com>
2023-05-03 14:49:20 -0400

    react changes

--- posv2-react/src/App.js
+++ posv2-react/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from './Navbar';
+import Kitchen from './Kitchen';
+import api from './api/axiosConfig';
+import {useState,useEffect} from 'react';
+import Layout from "./components/Layout";
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
+
+  const[floor,setTables] = useState();
+  const[table,setTable] = useState();
+
+  const getTables = async () =>{
+    try{
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setTables(response.data);
+
+    } catch(err){
+      console.log(err);
+    }
+  }
+
+  const getTableData = async (tableID) => {
+    try{
+      const response = await api.get('/api/v1/tables/${tableID}');
+      const singleTable = response.data;
+      setTable(singleTable);
+
+    }catch(error) {
+
+    }
+  }
+
+  useEffect(() => {
+    getTables();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          
+           <Route path="/" element = {<Layout/>}>
+            <Route path="/"element= {<Home/>}> 
+            </Route>
+            </Route>
+
+            <Route path="/Kitchen">
+              <Kitchen />
+            </Route>
+
+        </div>
+      </div>
+    </Routes>
   );
 }
 
-export default App;
+export default App
